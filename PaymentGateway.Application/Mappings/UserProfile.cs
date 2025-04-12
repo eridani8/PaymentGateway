12d3a817ae9b809efd8368592882d513b4b3f6ad
Eridani 8 <tsuuzetsu@gmail.com>
2025-04-12 18:13:47 +0300

    added user grid

--- PaymentGateway.Application/Mappings/UserProfile.cs
+++ PaymentGateway.Application/Mappings/UserProfile.cs
@@ -9,7 +9,9 @@ public class UserProfile : Profile
     public UserProfile()
     {
         CreateMap<UserEntity, UserDto>()
-            .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.UserName));
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.UserName))
+            .ForMember(dest => dest.Roles, opt => opt.MapFrom(src => src.Roles));
 
         CreateMap<CreateUserDto, UserEntity>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.CreateVersion7()))
--- PaymentGateway.Shared/DTOs/User/UserDto.cs
+++ PaymentGateway.Shared/DTOs/User/UserDto.cs
@@ -2,5 +2,7 @@
 
 public class UserDto
 {
+    public Guid Id { get; init; }
     public string Username { get; init; } = string.Empty;
+    public List<string> Roles { get; init; } = [];
 }
