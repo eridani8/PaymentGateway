eea122b06bfae0ec375f2c85e8e7e53b911b679a
Eridani 8 <tsuuzetsu@gmail.com>
2025-04-15 15:04:34 +0300

    update

---
+++ PaymentGateway.Shared/Constants/SignalREvents.cs
@@ -0,0 +1,9 @@
+namespace PaymentGateway.Shared.Constants;
+
+public static class SignalREvents
+{
+    public const string RequisiteUpdated = "RequisiteUpdated";
+    public const string RequisiteDeleted = "RequisiteDeleted";
+    public const string UserUpdated = "UserUpdated";
+    public const string UserDeleted = "UserDeleted";
+} 
