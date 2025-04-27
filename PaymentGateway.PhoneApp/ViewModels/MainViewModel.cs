f47cd064b3d3c9725b3c3d80df0ca68f6db87d82
Eridani 8 <tsuuzetsu@gmail.com>
2025-04-27 16:05:25 +0300

    update

--- PaymentGateway.PhoneApp/ViewModels/MainViewModel.cs
+++ PaymentGateway.PhoneApp/ViewModels/MainViewModel.cs
@@ -1,22 +1,6 @@
-using CommunityToolkit.Maui.Alerts;
-using CommunityToolkit.Mvvm.Input;
-
 namespace PaymentGateway.PhoneApp.ViewModels;
 
 public partial class MainViewModel : BaseViewModel
 {
-    [RelayCommand]
-    private async Task ShowToast()
-    {
-        try
-        {
-            IsBusy = true;
-            await Toast.Make("hello").Show();
-            await Task.Delay(2000);
-        }
-        finally
-        {
-            IsBusy = false;
-        }
-    }
+    
 }
