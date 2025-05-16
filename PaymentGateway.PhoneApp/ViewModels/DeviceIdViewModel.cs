85b33f10c6c6ccb90dbdf7e008542a837bbae540
Eridani 8 <tsuuzetsu@gmail.com>
2025-05-17 02:01:47 +0300

    🔧 refactor(ViewModels/DeviceIdViewModel): simplify constructor by using IDeviceService instead of DeviceService

--- PaymentGateway.PhoneApp/ViewModels/DeviceIdViewModel.cs
+++ PaymentGateway.PhoneApp/ViewModels/DeviceIdViewModel.cs
@@ -1,15 +1,11 @@
-using System.Globalization;
 using CommunityToolkit.Maui.Alerts;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using LiteDB;
 using PaymentGateway.PhoneApp.Interfaces;
-using PaymentGateway.PhoneApp.Services;
-using PaymentGateway.PhoneApp.Types;
 
 namespace PaymentGateway.PhoneApp.ViewModels;
 
-public partial class DeviceIdViewModel(DeviceService deviceService) : BaseViewModel
+public partial class DeviceIdViewModel(IDeviceService deviceService) : BaseViewModel
 {
     [ObservableProperty] private string _deviceId = deviceService.DeviceId.ToString();
 
